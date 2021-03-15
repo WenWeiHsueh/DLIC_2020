4a33af7ccbf337151b0f9ab4d90829e180a0ca73
Wei Cheng <michael1996623@gmail.com>
2021-03-16 00:39:15 +0800

    fix-Move define macro to diff file

---
+++ hw5_Conv/src/def.v
@@ -0,0 +1,39 @@
+// This is generated automatically on 2021/03/16-00:36:36
+// Check the # of bits for state registers !!!
+// Check the # of bits for flag registers !!!
+
+`ifndef __FLAG_DEF__
+`define __FLAG_DEF__
+
+// There're 8 flags in this design
+`define F_GEN_IN_ADDR          	 0  
+`define F_READ_IN_ENB          	 1  
+`define F_CONV_RELU_ENB        	 2  
+`define F_WRITE_CONV_ENB       	 3  
+`define F_GEN_CONV_ADDR        	 4  
+`define F_READ_CONV_ENB        	 5  
+`define F_WRITE_POOL_ENB       	 6  
+`define F_WRITE_FLAT_ENB       	 7  
+`define FLAG_WIDTH             	 8  
+
+// There're 7 states in this design
+`define S_READY                	 0  
+`define S_READ_WEIGHT          	 1  
+`define S_READ_INPUT           	 2  
+`define S_MULTIPLY             	 3  
+`define S_ADD                  	 4  
+`define S_WRITE                	 5  
+`define S_FINISH               	 6  
+`define S_INIT                 	 7'b0
+`define STATE_WIDTH            	 7  
+
+// Other macro in this design
+`define IN_BUFFER_SIZE         	 8'd66
+`define OUT_BUFFER_SIZE        	 8'd64
+`define READ_MEM_DELAY         	 2'd2
+`define EMPTY_ADDR             	 {12{1'b0}}
+`define EMPTY_DATA             	 {20{1'b0}}
+`define LOCAL_IDX_WIDTH        	 16 
+`define DATA_WIDTH             	 20 
+
+`endif
---
+++ hw5_Conv/src/gen_def.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+timestamp=$(date +%Y/%m/%d-%H:%M:%S)
+printf "// This is generated automatically on ${timestamp}\n"
+printf "// Check the # of bits for state registers !!!\n"
+printf "// Check the # of bits for flag registers !!!\n\n"
+
+STATES=("S_READY"            \
+        "S_READ_WEIGHT"      \
+        S_READ_INPUT""       \
+        "S_MULTIPLY"         \
+        "S_ADD"              \
+        "S_WRITE"            \
+        "S_FINISH")
+
+FLAGS=("F_GEN_IN_ADDR"      \
+       "F_READ_IN_ENB"      \
+       "F_CONV_RELU_ENB"    \
+       "F_WRITE_CONV_ENB"   \
+       "F_GEN_CONV_ADDR"    \
+       "F_READ_CONV_ENB"    \
+       "F_WRITE_POOL_ENB"   \
+       "F_WRITE_FLAT_ENB")
+
+def_pattern="%-30s \t %-3s\n"
+
+# Generate macro
+printf "\`ifndef __FLAG_DEF__\n"
+printf "\`define __FLAG_DEF__\n"
+
+# Generate flags
+len=${#FLAGS[@]}
+printf "\n// There're ${len} flags in this design\n"
+for((idx=0; idx<${len}; idx++))
+do
+  printf "$def_pattern" "\`define ${FLAGS[$idx]}" "${idx}"
+done
+
+# Generate flag width
+printf "$def_pattern" "\`define FLAG_WIDTH" "`expr ${idx}`"
+
+# Generate FSM states
+len=${#STATES[@]}
+printf "\n// There're ${len} states in this design\n"
+for((idx=0; idx<${len}; idx++))
+do
+  printf "$def_pattern" "\`define ${STATES[$idx]}" "${idx}"
+done
+
+# Generate FSM init vector
+printf "$def_pattern" "\`define S_INIT" "${len}'b0"
+
+# Generate state width
+printf "$def_pattern" "\`define STATE_WIDTH" "`expr ${idx}`"
+
+# Generate other macro
+printf "\n// Other macro in this design\n"
+IN_BUFFER_SIZE="8'd66"
+OUT_BUFFER_SIZE="8'd64"
+printf "$def_pattern" "\`define IN_BUFFER_SIZE"       "$IN_BUFFER_SIZE"
+printf "$def_pattern" "\`define OUT_BUFFER_SIZE"      "$OUT_BUFFER_SIZE"
+printf "$def_pattern" "\`define READ_MEM_DELAY"       "2'd2"
+
+printf "$def_pattern" "\`define EMPTY_ADDR"           "{12{1'b0}}"
+printf "$def_pattern" "\`define EMPTY_DATA"           "{20{1'b0}}"
+
+printf "$def_pattern" "\`define LOCAL_IDX_WIDTH"      "16"
+printf "$def_pattern" "\`define DATA_WIDTH"           "20"
+
+# Generate end macro
+printf "\n\`endif\n"
