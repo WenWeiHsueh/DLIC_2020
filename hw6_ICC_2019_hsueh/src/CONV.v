885725d314dd79be41cad7f81839bfa829d0f902
WenWeiHsueh <F74086373@gs.ncku.edu.tw>
2021-09-22 03:40:18 +0800

    START_MY_ICC_2019

---
+++ hw6_ICC_2019_hsueh/src/CONV.v
@@ -0,0 +1,40 @@
+
+`timescale 1ns/10ps
+
+module  CONV(
+	input		clk,
+	input		reset,
+	output		busy,	
+	input		ready,	
+			
+	output		iaddr,
+	input		idata,	
+	
+	output	 	cwr,
+	output	 	caddr_wr,
+	output	 	cdata_wr,
+	
+	output	 	crd,
+	output	 	caddr_rd,
+	input	 	cdata_rd,
+	
+	output	 	csel
+	);
+
+
+
+endmodule
+
+module ctrl(
+	input clk,
+	input reset,
+
+);
+
+module dp(
+	input clk,
+	input reset,
+
+);
+
+
---
+++ hw6_ICC_2019_hsueh/src/fakeMem.v
@@ -0,0 +1,36 @@
+// This module translate the zero-padded 66x66 input memory index
+// to the real 64x64 input memory index. By doing so, we don't need
+// to "actually" do zero-padding
+
+`include "def.v"
+module fakeMem (
+           input   wire    clk,
+           input   wire    [`LOCAL_IDX_WIDTH-1:0] pseudo_addr,
+           output  reg     [11:0] iaddr,
+           output  reg     zero_flag
+       );
+
+// assume `LOCAL_IDX_WIDTH = 16
+wire [2*`LOCAL_IDX_WIDTH-1:0] tmp = pseudo_addr * 16'h03E1;
+wire [`LOCAL_IDX_WIDTH-1:0] quotient = tmp >> 16;
+wire [7:0] mod66 = (pseudo_addr - (quotient * 66));
+
+//wire [7:0] mod66 = pseudo_addr % 66;
+//wire [`LOCAL_IDX_WIDTH-1:0] quotient = (pseudo_addr - mod66) / 66;
+
+wire mod_cond = (mod66 == 0) || (mod66 == 65);
+wire zero_cond = ((pseudo_addr >= 0) && (pseudo_addr <= 65)) || ((pseudo_addr >= 4290) && (pseudo_addr <= 4355)) || mod_cond;
+
+
+always @(*) begin
+    if(zero_cond) begin
+        zero_flag = 1;
+        iaddr = `EMPTY_ADDR; // sending nothing to address bus
+    end
+    else begin
+        zero_flag = 0;
+        iaddr = pseudo_addr - 67 - 2 * (quotient-1);
+    end
+end
+
+endmodule
